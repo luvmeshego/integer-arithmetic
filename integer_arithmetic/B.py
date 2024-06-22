14abe4eae11b5aff521fd63a34224d83dacd8325
luvmeshego <dimasrex10@gmail.com>
2024-06-22 14:36:21 +0300

    Add files via upload

---
+++ integer_arithmetic/B.py
@@ -0,0 +1,2 @@
+number = int(input())
+print(f'The next number for the number {number} is {number + 1}.\nThe previous number for the number {number} is {number - 1}.')
---
+++ integer_arithmetic/G.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = a // 100
+c = (a % 100) // 10
+d = a % 10
+print(b + c + d)
