a776fd1d37473029c6cf96857fc41c99f15dbbe9
luvmeshego <dimasrex10@gmail.com>
2024-07-16 16:21:01 +0300

    Add files via upload

--- integer_arithmetic/P.py
+++ integer_arithmetic/P.py
@@ -1,5 +1,7 @@
 n = int(input())
-sec = n % 60
-minute = n  % 60
+sec = (n % 60)
+minute = (n // 60) % 60
 hour = (n // 3600) % 24
-print('0' hour,':', minute,':', sec)
+ost_sec = (sec % 100) // 10
+ost_min = (minute % 100) // 10
+print(str(hour) + ':' + str(ost_min) + str(minute % 10) + ':' + str(ost_sec) + str(sec % 10))
