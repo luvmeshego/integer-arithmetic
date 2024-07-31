6acd01cfb733c2b39151bfce20093b61678e460b
luvmeshego <dimasrex10@gmail.com>
2024-07-31 20:03:07 +0300

    Update U.py

--- integer_arithmetic/U.py
+++ integer_arithmetic/U.py
@@ -1,8 +1,11 @@
 N = int(input())
 M = int(input())
-maximum = ((N - M) // 2) * (N - M + 1)
-more = N % M
-less = M - more
+maximum = ((N - M) / 2) * (N - M + 1)
+teams_with_more_people = N % M
+teams_with_less_people = M - teams_with_more_people
 less_quantity = N // M
-minimum = (N // M - 1) * less
-print(minimum, maximum)
+more_quantity = less_quantity + 1
+less_team_handshakes = (less_quantity - 1) / 2 * less_quantity
+more_team_handshakes = (more_quantity - 1) / 2 * more_quantity
+minimum = (less_team_handshakes * teams_with_less_people) + (more_team_handshakes * teams_with_more_people)
+print(int(minimum), int(maximum))
